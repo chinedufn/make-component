f2f62f18a10e7b104831552ac68549c6ee895baf
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2015-12-08 19:39:06 -0500

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 var fs = require('fs')
+var path = require('path')
 var makeComponentNode = require('../src/make-node.js')
 var makeComponentStyle = require('../src/make-style.js')
 var test = require('tape')
 
-var componentNode = fs.readFileSync('./fixture/some-component.js', 'utf8')
-var componentStyle = fs.readFileSync('./fixture/some-component_style.js', 'utf8')
+var componentNode = fs.readFileSync(path.resolve(__dirname, './fixture/some-component.js'), 'utf8')
+var componentStyle = fs.readFileSync(path.resolve(__dirname, './fixture/some-component_style.js'), 'utf8')
 
 test(function (t) {
   t.equal(makeComponentNode('some-component') + '\n', componentNode)

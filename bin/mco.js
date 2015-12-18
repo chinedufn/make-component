79d187934233b1a07a7dedea04a750815193b9df
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2015-12-18 11:52:15 -0500

    Add -d command
    Generates a directory with a node and style file

--- bin/mco.js
+++ bin/mco.js
@@ -5,6 +5,9 @@
 var meow = require('meow')
 var makeNode = require('../src/make-node.js')
 var makeStyle = require('../src/make-style.js')
+var mkdirp = require('mkdirp')
+
+var fs = require('fs')
 
 var cli = meow({
   help: [
@@ -14,8 +17,23 @@ var cli = meow({
 })
 
 var componentName = cli.input[0]
-if (cli.flags.s) {
-  console.log(makeStyle(componentName))
+
+var nodeFilename = componentName + '.js'
+var styleFilename = componentName + '_style.js'
+
+var node = makeNode(componentName)
+var style = makeStyle(componentName)
+
+if (cli.flags.d) {
+  mkdirp.sync(componentName)
+  fs.writeFile(componentName + '/' + nodeFilename, node, function (err) {
+    if (err) throw err
+  })
+  fs.writeFile(componentName + '/' + styleFilename, style, function (err) {
+    if (err) throw err
+  })
+} else if (cli.flags.s) {
+  console.log(style)
 } else {
-  console.log(makeNode(componentName))
+  console.log(node)
 }

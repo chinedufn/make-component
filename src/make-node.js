e6ff21f13e52a8bde16f500ab093945847e3b02b
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-02-18 06:26:17 -0500

    Require `h` func to be passed into render func

--- src/make-node.js
+++ src/make-node.js
@@ -7,20 +7,16 @@ function MakeNode (componentName) {
   var uppercaseName = camelCasedName.charAt(0).toUpperCase() + camelCasedName.slice(1)
 
   var componentNode = '' +
-  "var h = require('virtual-dom/h')\n\n" +
-
   "var style = require('./" + componentName + "_style.js')\n\n" +
 
   'module.exports = {\n' +
   '  render: Render' + uppercaseName + '\n' +
   '}\n\n' +
 
-  'function Render' + uppercaseName + ' (viewport) {\n' +
+  'function Render' + uppercaseName + ' (h, viewport, opts) {\n' +
   '  var ' + camelCasedName + 'Style = style[viewport] || style.base\n\n' +
   '  var rendered' + uppercaseName + " = h('div', {\n" +
-  '    attributes: {\n' +
-    '      filepath: __filename\n' +
-  '    },\n' +
+  '    attributes: { filepath: __filename },\n' +
   '    style: ' + camelCasedName + 'Style\n' +
   '  }, [\n' +
   '  ])\n\n' +
--- test/fixture/some-component.js
+++ test/fixture/some-component.js
@@ -1,18 +1,14 @@
-var h = require('virtual-dom/h')
-
 var style = require('./some-component_style.js')
 
 module.exports = {
   render: RenderSomeComponent
 }
 
-function RenderSomeComponent (viewport) {
+function RenderSomeComponent (h, viewport, opts) {
   var someComponentStyle = style[viewport] || style.base
 
   var renderedSomeComponent = h('div', {
-    attributes: {
-      filepath: __filename
-    },
+    attributes: { filepath: __filename },
     style: someComponentStyle
   }, [
   ])
